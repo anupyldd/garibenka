e27a419ec07308e0b02de5dd9e612df418292ec2
anupyldd <139476397+anupyldd@users.noreply.github.com>
2023-08-12 15:33:05 +0300

    add loading from PyOutput into allFileContents vector

--- Garibenka/FileLoader.h
+++ Garibenka/FileLoader.h
@@ -10,57 +10,71 @@
 #include <sstream>
 #include <algorithm>
 #include <unordered_map>
+#include <iostream>
+#include <fstream>
+#include <iterator>
 
 #define PY_SSIZE_T_CLEAN
 
-class FileLoader
+class Symbol
 {
 private:
-	
-	class Symbol
-	{
-	private:
-		std::string symbol = "phSymbol";
-		std::string reading = "phReading";
-		std::string meaning = "phMeaning";
-		bool isLearnt = false;
-
-	public:
-		Symbol(std::string inSymbol, std::string inReading, std::string inMeaning);
-		
-		virtual std::string_view GetSymbol() = 0;
-		virtual std::string_view GetReading() = 0;
-		virtual std::string_view GetMeaning() = 0;
-
-		virtual void SetIsLearnt() = 0;
-	};
-
-	class Module
-	{
-	private:
-		std::string fromFile = "phFile";
-		std::string moduleName = "phModule";
-		std::vector<Symbol> wordList;
-		std::vector<Symbol> kanjiList;
-
-	public:
-		Module(std::string inFromFile, std::string inModuleName);
-
-		virtual void AddToWords() = 0;
-		virtual void AddToKanji() = 0;
-
-		virtual std::string_view GetFileName() = 0;
-		virtual std::string_view GetModuleName() = 0;
-	};
-
+	std::string symbol = "phSymbol";
+	std::string reading = "phReading";
+	std::string meaning = "phMeaning";
+	bool isLearnt = false;
 
 public:
-	//std::unordered_map<std::string, std::string> 
+	Symbol(std::string inSymbol, std::string inReading, std::string inMeaning);
+
+	std::string_view GetSymbol();
+	std::string_view GetReading();
+	std::string_view GetMeaning();
+
+	void SetIsLearnt(bool inIsLearnt);
+};
+
+
+class Module
+{
+private:
+	std::string fromFile = "phFile";
+	std::string moduleName = "phModule";
+	std::vector<Symbol> wordList;
+	std::vector<Symbol> kanjiList;
+
+public:
+	Module(std::string inFromFile, std::string inModuleName);
+
+	void AddToWords(Symbol& inWord);
+	void AddToKanji(Symbol& inKanji);
+
+	std::string_view GetFileName();
+	std::string_view GetModuleName();
+};
+
+
+
+
+class FileHandler
+{
+
+public:
 
 	static void RunPythonScript(int argc, const char* argv[]);
+
+	static void Split(const std::string& s, char delim, std::vector<std::string>& elems);
 	static void ReadTablesFile();
 
 	static void ReadUserSettingsFile();
 
+	/*std::string_view GetSymbol();
+	std::string_view GetReading();
+	std::string_view GetMeaning();
 
+	void AddToWords();
+	void AddToKanji();
+
+	std::string_view GetFileName();
+	std::string_view GetModuleName();*/
 };
